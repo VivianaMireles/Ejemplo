0022e7d9539a759ba118853438064087c012b3ff
viviana <viviana.mireles93@gmail.com>
2018-02-01 11:43:06 -0700

    agregar read

---
+++ resources/assets/js/components/vistasComponentes/read.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="read">
+
+    <section>
+      <headerRead></headerRead>
+    </section>
+
+    <section class="container">
+      <h1>Lee me BEBE!!!;D</h1>
+    </section>
+
+  </div>
+</template>
+<script>
+import headerRead from "./common/portadaRead.vue"
+
+export default{
+  components: {
+    headerRead
+    },
+    data(){
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
